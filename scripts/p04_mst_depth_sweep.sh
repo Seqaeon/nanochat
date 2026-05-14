41dee51def588dd418e2f052555e73d268b30e77
Seqaeon <77976875+Seqaeon@users.noreply.github.com>
2026-05-14 17:04:05 +0100

    meh

--- scripts/p04_mst_depth_sweep.sh
+++ scripts/p04_mst_depth_sweep.sh
@@ -129,7 +129,7 @@ run_experiment() {
 
     # Common training flags
     local MST_COMMON="--models base \
-      --device-batch-size 128 --total-batch-size -1 --use-onecycle 0 --log-every 200 --skip-core \
+      --device-batch-size 64 --total-batch-size -1 --use-onecycle 0 --log-every 200 --skip-core \
       --data-dir ${DATA_DIR:-data} --tokenizer-dir ${TOKENIZER_DIR:-tokenizer} \
       --sequence-len 2048 \
       --target-param-data-ratio 10.5 \
