05af6145095e2c19be0d97e917c4410c9fdffc1e
Seqaeon <77976875+Seqaeon@users.noreply.github.com>
2026-09-07 05:05:50 +0100

    meh

--- scripts/eet_p02_tests.sh
+++ scripts/eet_p02_tests.sh
@@ -137,7 +137,7 @@ init_state
 # order as the effect being measured.
 EET_COMMON="--models base \
   --device-batch-size ${DEVICE_BATCH_SIZE:-128} --total-batch-size -1 \
-  --use-onecycle 0 --log-every 20 --skip-core \
+  --use-onecycle 0 --log-every 200 --skip-core \
   --data-dir ${DATA_DIR:-data} --tokenizer-dir ${TOKENIZER_DIR:-tokenizer} \
   --sequence-len 2048 \
   --target-param-data-ratio 10.5 \
